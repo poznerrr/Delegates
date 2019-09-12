2cf9d269bbf97f6074c624b7da8a31fc9b4478a8
pozner <poznerrr@gmail.com>
2019-09-12 21:49:53 +0300

    Add project files.

---
+++ Delegate/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    class Fighter {
+        public void Punch() {
+            Console.WriteLine("I'm punching");
+        }
+        public void Kick() {
+            Console.WriteLine("I'm kicking");
+        }
+    }
+
+    public delegate void KomboHit();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fighter fighter = new Fighter();
+            KomboHit hit = new KomboHit(fighter.Kick);
+            hit += fighter.Punch;
+
+        }
+    }
+}
